khizkhiz/swift

commit a1f6040c83d62a3ec354bba398f902b66154ec41

[cmpcodesize] Add some tests for listFunctionSizes
Add unit tests to cmpcodesize. As a basic starting point for unit
tests, add some tests for edge cases in listFunctionSizes.

Users may now run the unit tests for cmpcodesize by running
`python utils/cmpcodesize/setup.py test`.

utils/cmpcodesize/cmpcodesize/compare.py
@@ -192,7 +192,7 @@ def listFunctionSizes(sizeArray):
     for pair in sorted(sizeArray, key=itemgetter(1)):
         name = pair[0]
         size = pair[1]
-        print "%8d %s" % (size, name)
+        return "%8d %s" % (size, name)
 
 
 def compareFunctionSizes(oldFiles, newFiles):
@@ -227,13 +227,13 @@ def compareFunctionSizes(oldFiles, newFiles):
 
     if onlyInFile1:
         print "Only in old file(s)"
-        listFunctionSizes(onlyInFile1)
+        print listFunctionSizes(onlyInFile1)
         print "Total size of functions only in old file: {}".format(onlyInFile1Size)
         print
 
     if onlyInFile2:
         print "Only in new files(s)"
-        listFunctionSizes(onlyInFile2)
+        print listFunctionSizes(onlyInFile2)
         print "Total size of functions only in new file: {}".format(onlyInFile2Size) 
         print
 

utils/cmpcodesize/cmpcodesize/main.py
@@ -166,7 +166,7 @@ How to specify files:
             sizes = collections.defaultdict(int)
             for file in oldFiles:
                 readSizes(sizes, file, True, False)
-            listFunctionSizes(sizes.items())
+            print listFunctionSizes(sizes.items())
         else:
             compareFunctionSizes(oldFiles, newFiles)
     else:

utils/cmpcodesize/tests/test_list_function_sizes.py
@@ -1,0 +1,16 @@
+import unittest
+
+from cmpcodesize.compare import listFunctionSizes
+
+
+class ListFunctionSizesTestCase(unittest.TestCase):
+    def test_when_size_array_is_none_raises(self):
+        with self.assertRaises(TypeError):
+            listFunctionSizes(None)
+
+    def test_when_size_array_is_empty_returns_none(self):
+        self.assertIsNone(listFunctionSizes([]))
+
+
+if __name__ == '__main__':
+    unittest.main()

utils/cmpcodesize/setup.py
@@ -20,6 +20,8 @@ setuptools.setup(
     description="A tool to compare the size of Swift compiler build products.",
     keywords='compare size swift',
 
+    test_suite='tests',
+
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',

commit 89dd75b2bb2b1dbc0e25ffaa1af576ecc56691f6

[cmpcodesize] Fix early return in --list
Fixes a bug, introduced in a1f6040c83d62a3ec354bba398f902b66154ec41,
in which `cmpcodesize -l /path/to/file` would only output the *first* label,
instead of listing all of them.

utils/cmpcodesize/cmpcodesize/compare.py
@@ -194,7 +194,7 @@ def listFunctionSizes(sizeArray):
     for pair in sorted(sizeArray, key=itemgetter(1)):
         name = pair[0]
         size = pair[1]
-        return "%8d %s" % (size, name)
+        yield "%8d %s" % (size, name)
 
 
 def compareFunctionSizes(oldFiles, newFiles):

utils/cmpcodesize/cmpcodesize/main.py
@@ -168,7 +168,7 @@ How to specify files:
             sizes = collections.defaultdict(int)
             for file in oldFiles:
                 readSizes(sizes, file, True, False)
-            print(listFunctionSizes(sizes.items()))
+            print(os.linesep.join(listFunctionSizes(sizes.items())))
         else:
             compareFunctionSizes(oldFiles, newFiles)
     else:

utils/cmpcodesize/tests/test_list_function_sizes.py
@@ -6,11 +6,22 @@ from cmpcodesize.compare import listFunctionSizes
 class ListFunctionSizesTestCase(unittest.TestCase):
     def test_when_size_array_is_none_raises(self):
         with self.assertRaises(TypeError):
-            listFunctionSizes(None)
+            list(listFunctionSizes(None))
 
     def test_when_size_array_is_empty_returns_none(self):
-        self.assertIsNone(listFunctionSizes([]))
+        self.assertEqual(list(listFunctionSizes([])), [])
 
+    def test_lists_each_entry(self):
+        sizes = {
+            'foo': 1,
+            'bar': 10,
+            'baz': 100,
+        }
+        self.assertEqual(list(listFunctionSizes(sizes.items())), [
+            '       1 foo',
+            '      10 bar',
+            '     100 baz',
+        ])
 
 if __name__ == '__main__':
     unittest.main()